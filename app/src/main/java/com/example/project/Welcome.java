766825210dc5110531a78ef208da49628b7f16b5
Arslan Ahmad <ars.ahm6@gmail.com>
2022-05-23 13:10:57 +0500

    New Successful Branch without Admob

--- app/src/main/java/com/example/project/Welcome.java
+++ app/src/main/java/com/example/project/Welcome.java
@@ -8,11 +8,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdError;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.FullScreenContentCallback;
-import com.google.android.gms.ads.interstitial.InterstitialAd;
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -22,12 +17,11 @@ public class Welcome extends AppCompatActivity {
     Button logout, addStock, viewStock, addOrder, viewHistory;
     FirebaseAuth mAuth;
 
-    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         setContentView(R.layout.activity_welcome);
 
@@ -61,61 +55,7 @@ public class Welcome extends AppCompatActivity {
 
             // load ad when the activity get created
 
-            loadInterstitialAd();       // load interstitial ad
-
-            if (mInterstitialAd != null) {
-                mInterstitialAd.show(Welcome.this);       // show ad
-
-                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
-                    @Override
-                    public void onAdDismissedFullScreenContent() {
-                        // Called when fullscreen content is dismissed.
-                        Log.d("TAG", "The ad was dismissed.");
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onAdFailedToShowFullScreenContent(AdError adError) {
-                        // Called when fullscreen content failed to show.
-                        Log.d("TAG", "The ad failed to show.");
-                    }
-
-                    @Override
-                    public void onAdShowedFullScreenContent() {
-                        // Called when fullscreen content is shown.
-                        // Make sure to set your reference to null so you don't
-                        // show it a second time.
-                        mInterstitialAd = null;
-                        Log.d("TAG", "The ad was shown.");
-                    }
-                });
-
-
-
-            } else {
-                Log.d("TAG", "The interstitial ad wasn't ready yet.");
-            }
-
-            //startActivity(intent);
         });
-
-
-    }
-
-    // load interstitial ad
-    void loadInterstitialAd() {
-        AdRequest adRequest = new AdRequest.Builder().build();
-
-        InterstitialAd.load(this, getString(R.string.interstitial_ad_unit_id), adRequest,
-                new InterstitialAdLoadCallback() {
-                    @Override
-                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                        // The mInterstitialAd reference will be null until
-                        // an ad is loaded.
-                        mInterstitialAd = interstitialAd;
-                        Log.i("TAG", "onAdLoaded");
-                    }
-                });
     }
 
 
