a505463eecf0f64b59f4732f54572c0d46a3c813
muneebkhan4 <muneebkhan4@outlook.com>
2022-05-24 15:55:46 +0500

    Muneeb's eight commit, admob banner and interstitial ads added successfully.... Updated Code merged successfully

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     Button login;
     Button signup;
+
+    AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +32,21 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.main_login);
         signup = findViewById(R.id.main_signup);
 
-        login.setOnClickListener((view) -> {
-            startActivity(new Intent(MainActivity.this, Login.class));
+        login.setOnClickListener((view) -> startActivity(new Intent(MainActivity.this, Login.class)));
+
+        signup.setOnClickListener((view) -> startActivity(new Intent(MainActivity.this, Signup.class)));
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+                // not essential for now
+            }
         });
 
-        signup.setOnClickListener((view) -> {
-            startActivity(new Intent(MainActivity.this, Signup.class));
-        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     @Override
--- app/src/main/java/com/example/project/Welcome.java
+++ app/src/main/java/com/example/project/Welcome.java
@@ -8,6 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -17,11 +22,15 @@ public class Welcome extends AppCompatActivity {
     Button logout, addStock, viewStock, addOrder, viewHistory;
     FirebaseAuth mAuth;
 
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
+        mAuth=FirebaseAuth.getInstance();
+
+        // load ad when the activity get created
+        loadInterstitialAd();       // load interstitial ad
 
         setContentView(R.layout.activity_welcome);
 
@@ -53,9 +62,60 @@ public class Welcome extends AppCompatActivity {
             Intent intent = new Intent(Welcome.this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);              // remove all previous activities, efficient log out
 
-            // load ad when the activity get created
+            // showing ads
+            if (mInterstitialAd != null) {
+                mInterstitialAd.show(Welcome.this);       // show ad
 
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        // Called when fullscreen content is dismissed.
+                        Log.d("TAG", "The ad was dismissed.");
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                        // Called when fullscreen content failed to show.
+                        Log.d("TAG", "The ad failed to show.");
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        // Called when fullscreen content is shown.
+                        // Make sure to set your reference to null so you don't
+                        // show it a second time.
+                        mInterstitialAd = null;
+                        Log.d("TAG", "The ad was shown.");
+                    }
+                });
+
+
+
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.");
+            }
+
+            //startActivity(intent);
         });
+
+
+    }
+
+    // load interstitial ad
+    void loadInterstitialAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this, getString(R.string.interstitial_ad_unit_id), adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        Log.i("TAG", "onAdLoaded");
+                    }
+                });
     }
 
 
